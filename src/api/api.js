306174810f2ffb9e60b0d84a37dd03bbe9537a0a
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-12-09 16:31:59 +0300

    feat: create api class

---
+++ src/api/api.js
@@ -0,0 +1,36 @@
+class Api {
+  constructor(options) {
+    this._url = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  _handleOriginalResponse(res) {
+    if (!res.ok) {
+      return Promise.reject(`Error: ${res.status}`);
+    }
+    return res.json();
+  }
+
+  getNewsById(id) {
+    return fetch(`${this._url}/v0/item/${id}.json`, {
+      method: 'GET',
+      headers: this._headers
+    }).then(this._handleOriginalResponse)
+  }
+
+  getNewStoriesIds() {
+    return fetch(`${this._url}/v0/newstories.json`, {
+      method: 'GET',
+      headers: this._headers
+    }).then(this._handleOriginalResponse)
+  }
+}
+
+const api = new Api({
+  baseUrl: 'https://hacker-news.firebaseio.com',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+export default api;
