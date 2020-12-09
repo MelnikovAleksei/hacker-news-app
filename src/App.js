38978516fd03caf4d239536270b19ad642e1273e
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-12-09 16:39:11 +0300

    feat: create App component with routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import { Main } from './components/Main';
+import { ArticlesList } from './features/news/ArticlesList';
+import { SingleNewsPage } from './features/news/SingleNewsPage';
+import { Footer } from './components/Footer';
+import { PageNotFound } from './components/PageNotFound';
 
 function App() {
+
   return (
     <div className="App">
-      <h1>Hacker News App</h1>
+      <header>
+        <h1>Hacker news app</h1>
+      </header>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <React.Fragment>
+              <Main>
+                <ArticlesList />
+              </Main>
+            </React.Fragment>
+          )}
+        />
+        <Route exact path="/news/:newsId" component={SingleNewsPage}/>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+      <Footer />
     </div>
   );
 }
